af397669aa4503452b4b8a048bfd8d50b75b066c
sstempss <pro.player.egor@mail.ru>
2025-11-16 13:59:17 +0300

    Временное удаление команды admin для запуска бота

--- bot_deploy.py
+++ bot_deploy.py
@@ -174,7 +174,6 @@ class PassiveNFTBot:
             )
             # Регистрация обработчиков
             self.application.add_handler(CommandHandler("start", self.start_command))
-            self.application.add_handler(CommandHandler("adminserveraa", self.admin_command))
             self.application.add_handler(CallbackQueryHandler(self.subscription_callback, pattern="^subscription$"))
             self.application.add_handler(CallbackQueryHandler(self.subscription_plan_callback, pattern="^plan_"))
             self.application.add_handler(CallbackQueryHandler(self.payment_callback, pattern="^payment_"))
