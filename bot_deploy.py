3c01ba6b6f65da513a6f23660719306c11761ddd
sstempss <pro.player.egor@mail.ru>
2025-11-17 04:42:43 +0300

    патерн фикс

--- bot_deploy.py
+++ bot_deploy.py
@@ -194,7 +194,7 @@ class PassiveNFTBot:
             self.application.add_handler(CommandHandler("help", self.help_command))
             self.application.add_handler(CommandHandler("adminserveraa", self.admin_command))
             self.application.add_handler(CallbackQueryHandler(self.subscription_callback, pattern="^subscription$"))
-            self.application.add_handler(CallbackQueryHandler(self.subscription_plan_callback, pattern="^plan_"))
+            self.application.add_handler(CallbackQueryHandler(self.subscription_plan_callback, pattern="^subscription_plan_"))
             self.application.add_handler(CallbackQueryHandler(self.payment_callback, pattern="^payment_"))
             self.application.add_handler(CallbackQueryHandler(self.contact_callback, pattern="^contact$"))
             self.application.add_handler(CallbackQueryHandler(self.referral_callback, pattern="^referral$"))
