be56fefc96641e02b6e6b6420cc0e980feaca0c1
sstempss <pro.player.egor@mail.ru>
2025-11-20 00:39:31 +0300

    фикс кнопки оплатить

--- bot_deploy.py
+++ bot_deploy.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-PassiveNFT Bot - ВЕРСИЯ С АКТИВНЫМИ ПОДПИСКАМИ (за звездочки) - ИСПРАВЛЕННАЯ ВЕРСИЯ
+PassiveNFT Bot - ВЕРСИЯ С АКТИВНЫМИ ПОДПИСКАМИ (за звездочки) - ИСПРАВЛЕННАЯ ВЕРСИЯЯ
 """
 import asyncio
 import logging
